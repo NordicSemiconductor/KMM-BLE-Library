b94f117351005b1d94b66bedaa1aef2fe4a178f6
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2023-08-14 12:11:19 +0200

    Fix kapt issue

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,56 +1,31 @@
 plugins {
-    alias(libs.plugins.nordic.application)
-    kotlin("multiplatform")
-    id("org.jetbrains.compose")
-    kotlin("kapt")
-    alias(libs.plugins.hilt)
-}
-
-kotlin {
-    androidTarget()
-    sourceSets {
-
-        val androidMain by getting {
-            dependencies {
-//                configurations.get("kapt").dependencies.add(libs.hilt.compiler)
-                implementation(project(":shared"))
-
-                implementation(libs.hilt.android)
-
-                implementation(libs.nordic.blek.scanner)
-
-                implementation(libs.nordic.theme)
-                implementation(libs.nordic.navigation)
-                implementation(libs.nordic.permissions.ble)
-                implementation(libs.nordic.logger)
-
-                implementation(libs.kotlinx.coroutines.core)
-                implementation(libs.androidx.core.ktx)
-                implementation(libs.androidx.compose.material3)
-                implementation(libs.androidx.activity.compose)
-                implementation(libs.androidx.hilt.navigation.compose)
-                implementation(libs.androidx.compose.material.iconsExtended)
-            }
-        }
-    }
+    alias(libs.plugins.nordic.application.compose)
+    alias(libs.plugins.nordic.hilt)
 }
 
 group = "no.nordicsemi.android.kotlin.ble"
 
 android {
     namespace = "no.nordicsemi.android.kotlin.ble.app.mock"
-
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlin {
-        jvmToolchain(17)
-    }
 }
 
 dependencies {
-    configurations["kapt"].dependencies.add(project.dependencies.create("com.google.dagger:hilt-android-compiler:2.47"))
+    implementation(project(":shared"))
+
+    kapt(libs.hilt.compiler)
+    implementation(libs.hilt.android)
+
+    implementation(libs.nordic.blek.scanner)
+
+    implementation(libs.nordic.theme)
+    implementation(libs.nordic.navigation)
+    implementation(libs.nordic.permissions.ble)
+    implementation(libs.nordic.logger)
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.compose.material.iconsExtended)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("com.android.application").apply(false)
     id("com.android.library").apply(false)
     id("org.jetbrains.compose").apply(false)
+    alias(libs.plugins.nordic.application.compose).apply(false)
     alias(libs.plugins.nordic.application).apply(false)
     alias(libs.plugins.nordic.hilt).apply(false)
     alias(libs.plugins.hilt).apply(false)
